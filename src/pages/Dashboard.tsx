0ff38a13507a2330efa286da0677a6a663104f29
Dmytro Lobanov <dspwww@gmail.com>
2024-12-22 12:25:28 +0000

    Update Dashboard.tsx
     - Added 2 types of view for applications: table & cards
     - TODO: imprvs
     - TODO: create separates components

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,17 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { NavLink, useNavigate } from "react-router";
 import { getApplications } from "../services/applications";
 import useAuthStore from "../stores/useAuth";
 import { IApplication } from "../interfaces/applications";
 
+type ApplicationView = 'table' | 'card';
+
 function Dashboard() {
   const { user } = useAuthStore();
+  const [applicationsView, setApplicationsView] = useState<ApplicationView>('table')
   const navigate = useNavigate();
 
   const [applications, setApplications] = useState<IApplication[]>([]);
 
   const handleCreateNewApplication = () => {
     navigate('/application/new');
+  }
+
+  const onViewChange = (e:ChangeEvent<HTMLSelectElement>) => {
+    setApplicationsView(e.target.value as ApplicationView);
   }
 
   useEffect(() => {
@@ -41,8 +48,37 @@ function Dashboard() {
           </p>
 
           <div>
-            {applications.map((application, i) =>
-              <article key={`application${i}`}>
+            <div>
+              Switch between table and cards
+              <select name="view" aria-label="Select type of view for applications: table or cards" onChange={onViewChange} >
+                <option value="table">Table</option>
+                <option value="cards">Cards</option>
+              </select>
+            </div>
+
+            {applicationsView == 'table' && <table>
+              <thead>
+                <tr>
+                  <th scope="col">Position</th>
+                  <th scope="col">URL</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Company</th>
+                </tr>
+              </thead>
+              <tbody>
+                {applications.map((application, i) =>
+                  <tr key={`application-table${i}`}>
+                    <th scope="row">{application.position}</th>
+                    <td><a href={application.url} target="_blank">Vacancy</a></td>
+                    <td>{application.status && application.status}</td>
+                    <td>{application.companyName && application.companyName}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>}
+
+            {applicationsView == 'cards' && applications.map((application, i) =>
+              <article key={`application-card${i}`}>
                 {application.status && <p><small>{application.status}</small></p>}
                 {application.companyName && <p><small>{application.companyName}</small></p>}
                 <h4>{application.position}</h4>
