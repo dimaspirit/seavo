49169baee1280ca15ed2b5856521260d8baa9842
Dmytro Lobanov <dspwww@gmail.com>
2024-12-27 12:14:38 +0000

    Added list of statuses

---
+++ src/consts/statuses.ts
@@ -0,0 +1,36 @@
+const STATUSES = {
+  DRAFT: {
+    id: 0,
+    name: 'Draft',
+  },
+  SENT: {
+    id: 1,
+    name: 'Sent application',
+  },
+  UNDER_REVIEW: {
+    id: 2, 
+    name: 'Under review',
+  },
+  INTERVIEW_SCHEDULED: {
+    id: 3,
+    name: 'Interview scheduled',
+  },
+  ACCEPTED: {
+    id: 4,
+    name: 'Accepted',
+  },
+  DECLINED: {
+    id: 5, 
+    name: 'Declined',
+  },
+  REJECTED: {
+    id: 6,
+    name: 'Rejected',
+  },
+  NO_RESPONSE: {
+    id: 7,
+    name: 'No response',
+  },
+};
+
+export {STATUSES};
